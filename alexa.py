1d71d32f92851fc338d8cc741febe24ab7d2f2aa
antonio <sayuri13a@gmail.com>
2025-04-05 18:12:52 -0300

    On branch main
     Initial commit#
     Changes to be committed:
    
    	new file:   alexa.py
    	new file:   chat/chats.txt

---
+++ alexa.py
@@ -0,0 +1,9 @@
+import speech_recognition as sr 
+
+r = sr.Recognizer()
+with sr.Microphone () as s:
+    r.adjust_for_ambient_noise(s)
+    while True:
+        audio = r.listen(s)
+        speech= r.recognize_google(audio, language="pt-BR")
+        print('voce disse', speech)
